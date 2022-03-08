9049099e609d392ed21a4c56d817e2ca58c96a51
tobeh <tobiasch@gmx.at>
2022-03-08 19:09:47 +0100

    add movie navgrid

--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Sanitizer, SecurityContext } from '@angular/core';
+import { Component, OnDestroy, OnInit, Sanitizer, SecurityContext } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { MovieResponse, MovieResult } from 'moviedb-promise/dist/request-types';
@@ -10,25 +10,40 @@ import { TmdbAPIService } from '../tmdb-api.service';
   templateUrl: './movie.component.html',
   styleUrls: ['./movie.component.css']
 })
-export class MovieComponent implements OnInit {
+export class MovieComponent implements OnInit, OnDestroy {
 
   movieID!: number;
   playerURL: SafeResourceUrl = "";
   movie!: MovieResponse;
   cast?: string = "";
   genres?: string = "";
+  navgrid!: GridNav;
 
   constructor(private activatedRoute: ActivatedRoute, private tmdbService: TmdbAPIService, public sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
-    (document.querySelector("h1") as HTMLElement).focus();
+
+    /* get movie ID */
     this.activatedRoute.params.subscribe(async data => {
+
+      /* load all movie details and properties */
       this.movieID = Number(data["id"]);
       const info = await this.tmdbService.api.movieInfo(this.movieID);
       this.movie = info;
       this.playerURL = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.2embed.ru/embed/tmdb/movie?id=' + this.movie.id);
       this.cast = (await this.tmdbService.api.movieCredits(this.movieID)).cast?.slice(0,4).map(c => c.name + " (" + c.character + ")").join(", ");
       this.genres = this.movie.genres?.map(g => g.name).join(", ");
+
+      /* init nav grid */
+      this.navgrid = new GridNav("app-movie");
+      GridNav.navGrid!.stop();
+      GridNav.navGrid!.overflowRight = this.navgrid;
+      this.navgrid.overflowLeft = GridNav.navGrid;
+      this.navgrid.listen();
     });
+  }
+
+  ngOnDestroy(): void {
+    this.navgrid.stop();
   }
 }
